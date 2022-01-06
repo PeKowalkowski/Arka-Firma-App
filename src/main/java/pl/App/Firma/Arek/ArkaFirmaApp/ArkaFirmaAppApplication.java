b6f0e671d824a7d7b3bcf3b4337f98844595fb37
Paweł Kowalkowski <pekowalkowski@gmail.com>
2022-01-06 09:10:22 +0100

    Start App

---
+++ src/main/java/pl/App/Firma/Arek/ArkaFirmaApp/ArkaFirmaAppApplication.java
@@ -0,0 +1,13 @@
+package pl.App.Firma.Arek.ArkaFirmaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkaFirmaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkaFirmaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/App/Firma/Arek/ArkaFirmaApp/ArkaFirmaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.App.Firma.Arek.ArkaFirmaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkaFirmaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

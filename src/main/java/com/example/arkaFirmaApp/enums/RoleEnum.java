f8b611c9d88ffc7507b5bb4d13cf7c69c2c7ba21
Paweł Kowalkowski <pekowalkowski@gmail.com>
2022-07-29 16:53:34 +0200

    Created packages controllers, entities, enums, repositories, security config and services. Next added class User, Role and RoleEnum and configrated connection with database.

---
+++ src/main/java/com/example/arkaFirmaApp/enums/RoleEnum.java
@@ -0,0 +1,10 @@
+package com.example.arkaFirmaApp.enums;
+
+public enum RoleEnum {
+    ROLE_ADMIN,
+    ROLE_USER,
+    ROLE_TECHNOLOGIST,
+    ROLE_LOGISTICAN,
+    ROLE_SUPPLIER,
+    ROLE_PRODUCTION
+}

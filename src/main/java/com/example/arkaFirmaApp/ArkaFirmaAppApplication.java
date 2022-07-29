d369ec8818443f3e9afdaf2f9d4382d02a26de6c
Paweł Kowalkowski <pekowalkowski@gmail.com>
2022-07-29 15:23:47 +0200

    First Commit

---
+++ src/main/java/com/example/arkaFirmaApp/ArkaFirmaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.arkaFirmaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkaFirmaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArkaFirmaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/arkaFirmaApp/ArkaFirmaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arkaFirmaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkaFirmaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

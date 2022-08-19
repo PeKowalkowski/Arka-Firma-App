d048c867039171c3f775aaf0436a08f72b61e91c
Paweł Kowalkowski <pekowalkowski@gmail.com>
2022-08-19 16:32:52 +0200

    Created CRUD methods for LogisticanService. Created html pages for Logistican to display index, create and update projects.

--- src/main/java/com/example/arkaFirmaApp/repositories/LogisticanRepository.java
+++ src/main/java/com/example/arkaFirmaApp/repositories/LogisticanRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LogisticanRepository extends JpaRepository<Long, Logistican> {
+public interface LogisticanRepository extends JpaRepository<Logistican, Long> {
 }
--- src/main/java/com/example/arkaFirmaApp/services/LogisticanService.java
+++ src/main/java/com/example/arkaFirmaApp/services/LogisticanService.java
@@ -4,7 +4,12 @@ import com.example.arkaFirmaApp.entities.Logistican;
 import com.example.arkaFirmaApp.repositories.LogisticanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class LogisticanService {
@@ -12,16 +17,29 @@ public class LogisticanService {
     @Autowired
     private LogisticanRepository logisticanRepository;
 
-    public Page<Logistican> findPaginated(int pageNo, int pageSize, String sortField, String sortDir) {
-        return null;
+    public Page<Logistican> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection) {
+        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+                Sort.by(sortField).descending();
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
+        return this.logisticanRepository.findAll(pageable);
     }
 
     public void saveLogisticanProject(Logistican logistican) {
+        this.logisticanRepository.save(logistican);
     }
 
     public Logistican getLogisticanProjectById(Long id) {
+        Optional<Logistican> logisticanOptional = logisticanRepository.findById(id);
+        Logistican logistican = null;
+        if(logisticanOptional.isPresent()){
+            logistican = logisticanOptional.get();
+        }else {
+            throw new RuntimeException("Projekt o id : " + id + " nie istnieje.");
+        }
+        return logistican;
     }
 
     public void deleteLogisticanProject(Long id) {
+        logisticanRepository.deleteById(id);
     }
 }
